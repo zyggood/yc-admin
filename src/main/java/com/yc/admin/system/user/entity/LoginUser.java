683969c32af078f4d026451e3548ad2c7b062505
张有成 <zyggood@qq.com>
2025-08-23 16:27:26 +0800

    refactor: 移除LoginUser中多余的Serializable接口实现

--- src/main/java/com/yc/admin/system/user/entity/LoginUser.java
+++ src/main/java/com/yc/admin/system/user/entity/LoginUser.java
@@ -8,7 +8,6 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
-import java.io.Serializable;
 import java.util.Collection;
 import java.util.Objects;
 import java.util.Set;
